863cf86176726db5ffa2d4e4eece91686d268bb1
hanmengxuan <104689232+hanmengxuan@users.noreply.github.com>
2022-05-01 09:20:16 +0800

    Add files via upload

---
+++ OJ22.java
@@ -0,0 +1,39 @@
+public class OJ22 {
+    public static void main(String[] args) {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+
+        int t = input.nextInt();
+        int n ;
+
+
+
+        for (int i = 0; i < t; i++) {
+            n = input.nextInt();
+            long[][] a = new long[n][n];
+            for (int q= 0; q< n;q++) {
+                for (int j = 0; j <n; j++)
+                    a[q][j] = input.nextLong();
+            }
+            String b = Symmetric(a);
+            System.out.println(b);
+        }
+
+
+    }
+
+    public static String Symmetric(long[][] array) {
+
+        String a="Y";
+        String b="N";
+
+        for (int i = 0; i < array.length; i++) {
+            for(int j = 0;j<array[i].length;j++) {
+                if (array[i][j] != array[j][i])
+                    return b;
+
+            }
+        }
+        return a;
+    }
+}
+
